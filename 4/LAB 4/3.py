c6537d7582fe2056865fc7c421d4557d4151acfb
ABHISEK PANDA <117925314+abhisek2004@users.noreply.github.com>
2024-08-13 20:17:12 +0530

    Delete 4/LAB 4 directory

--- 4/LAB 4/3.py
+++
@@ -1,7 +0,0 @@
-# 3.Write a program to create an integer list of 20 elements and increase the odd valued elements by 5.
-
-integer_list = [i for i in range(1, 21, 2)]  # Generates a
-for i in range(len(integer_list)):
-    if integer_list[i] % 2 != 0:
-        integer_list[i] += 5
-print(integer_list)
--- 4/LAB 4/6.py
+++
@@ -1,7 +0,0 @@
-# 6.Write a program to enter a dictionary and remove the duplicate values inside the dictionary.
-
-my_dict = {'a': 1, 'b': 2, 'c': 1, 'd': 3}
-unique_values = list(set(my_dict.values()))
-unique_dict = {key: value for key,
-               value in my_dict.items() if value in unique_values}
-print(unique_dict)
